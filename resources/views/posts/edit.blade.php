37fc21a10eaafee9a6953a4414f24576139e64bc
Masaki1152 <takanenoharusaku@gmail.com>
2024-08-20 02:47:22 +0900

    feat:バリデーションの追加

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -7,11 +7,13 @@
             @method('PUT')
             <div class='content__title'>
                 <h2>タイトル</h2>
-                <input type='text' name='post[title]' value="{{ $post->title }}">
+                <input type="text" name="post[title]" value="{{ $post->title }}"/>
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             <div class='content__body'>
                 <h2>本文</h2>
-                <input type='text' name='post[body]' value="{{ $post->body }}">
+                <textarea name="post[body]" >{{ $post->body }}</textarea>
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
             <input type="submit" value="保存">
         </form>
