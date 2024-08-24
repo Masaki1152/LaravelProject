28b8515b2edf238bd1a42e520494efa967fb71fb
Masaki1152 <takanenoharusaku@gmail.com>
2024-08-25 01:22:26 +0900

    feat:ブログ名の表示を行う

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -22,9 +22,9 @@ class Post extends Model
         return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
     }
 
-    public function getPaginateByLimit(int $limit_count = 10)
+    public function getPaginateByLimit(int $limit_count = 5)
     {
-        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+        return $this::with('category')->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
 
     // Categoryに対するリレーション
